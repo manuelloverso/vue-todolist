8d4d5e25a8c10025d76498372ff143d69c309548
manuelloverso <manuelloverso@gmail.com>
2024-03-13 15:17:52 +0100

    add second milestone

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,14 @@ createApp({
         { text: "Haircut", done: true },
         { text: "Wash the car", done: false },
         { text: "Shopping at the supermarket", done: false },
-        { text: "Go to the doctor", done: false },
+        { text: "Go to the doctor", done: true },
       ],
     };
+  },
+
+  methods: {
+    removeTask(index) {
+      this.toDoList.splice(index, 1);
+    },
   },
 }).mount("#app");
