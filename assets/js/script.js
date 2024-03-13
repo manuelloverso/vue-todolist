aa7131c76f0441775e17c6e5fb467c642c284ebd
manuelloverso <manuelloverso@gmail.com>
2024-03-13 15:07:09 +0100

    add first milestone

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,13 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      newTask: "",
+      toDoList: [
+        { text: "Haircut", done: true },
+        { text: "Wash the car", done: false },
+        { text: "Shopping at the supermarket", done: false },
+        { text: "Go to the doctor", done: false },
+      ],
     };
   },
 }).mount("#app");
